5f214beb4152db3c5a32e398c0e5f06d1ab7c29c
moritz.dorner@student.htl-rankweil.at <moritz.dorner@student.htl-rankweil.at>
2024-12-09 07:18:43 +0000

    Created ServerModule1; Edited Form1

---
+++ server_code/ServerModule1.py
@@ -0,0 +1,12 @@
+import anvil.server
+
+
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+
+@anvil.server.callable
+def get_account_number(email, password):
+  
