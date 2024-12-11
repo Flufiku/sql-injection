d1132e3f484ab16aeadc2b6b76f3d5f7bc840da5
moritz.dorner@student.htl-rankweil.at <moritz.dorner@student.htl-rankweil.at>
2024-12-11 09:58:55 +0000

    Edited ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -9,4 +9,9 @@ import anvil.server
 
 @anvil.server.callable
 def get_account_number(email, password):
-  
+  conn = sqlite3.connect(data_files['gefaengnis.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute("SELECT Name FROM Gefaengnis"))
+  new_res = [(item[0], item[0]) for item in res]
+  conn.close()
+  return new_res
