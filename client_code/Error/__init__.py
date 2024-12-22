24dd1c5b4eebf5914f6e29e433efa36106b76c8f
moritz.dorner@student.htl-rankweil.at <moritz.dorner@student.htl-rankweil.at>
2024-12-22 13:24:16 +0000

    Edited settings and Error

--- client_code/Error/__init__.py
+++ client_code/Error/__init__.py
@@ -1,8 +1,7 @@
-from ._anvil_designer import ErrorPageTemplate
 from anvil import *
 from anvil_extras import routing
 
 @routing.error_form
-class ErrorPage(ErrorPageTemplate):
+class ErrorPage():
   def __init__(self, **properties):
     self.init_components(**properties)
